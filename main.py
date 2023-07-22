fe5a88dbad51fa29b5008ecb9b5fb826c48544f6
Amirul <amirulshafiq.dc@gmail.com>
2023-07-22 18:18:30 +0800

    test

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ frames = []
 for link in url:
     dr.get(link)
     soup = BeautifulSoup(dr.page_source,'lxml')
-    print(soup)
+    print(pd.DataFrame(soup))
 #     tableMain = soup.find_all('table', {'class':'table datatable-striped text-center equity_prices_table datatable-with-sneak-peek js-anchor-price-table d-none d-lg-block dataTable no-footer'})
 #     last_div = None
 #     for last_div in tableMain:pass
