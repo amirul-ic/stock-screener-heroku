664ab2fe3711bbb807d7758324f00f8339b4ae34
Amirul <amirulshafiq.dc@gmail.com>
2023-07-22 20:03:55 +0800

    test2

--- main.py
+++ main.py
@@ -19,45 +19,49 @@ dr = webdriver.Chrome()
 
 start = time.time()
 
-# #2) Create a loop to scrape info in each URL for each page.
+os.environ['http_proxy'] = os.environ.get('FIXIE_URL', '')
+os.environ['https_proxy'] = os.environ.get('FIXIE_URL', '')
 
-# url =[]
+#2) Create a loop to scrape info in each URL for each page.
 
-# for i in range(1,2):
-#     website_url = (f'https://www.bursamalaysia.com/market_information/equities_prices?page={i}&per_page=50')
-#     url.append(website_url)
+url =[]
+
+for i in range(1,2):
+    website_url = (f'https://www.bursamalaysia.com/market_information/equities_prices?page={i}&per_page=50')
+    url.append(website_url)
     
 
-# print (url)
+print (url)
 
-# import datetime
-# frames = []
+import datetime
+frames = []
 
-# for link in url:
-#     dr.get(link)
-#     soup = BeautifulSoup(dr.page_source,'lxml')
-#     tableMain = soup.find_all('table', {'class':'table datatable-striped text-center equity_prices_table datatable-with-sneak-peek js-anchor-price-table d-none d-lg-block dataTable no-footer'})
-#     last_div = None
-#     for last_div in tableMain:pass
-#     if last_div:
-#         table = last_div.getText()
-#     df = pd.read_html(str(last_div), header=0)
-#     df[0].rename(index= str, inplace = True)
-#     frames.append(df[0].assign(date=datetime.datetime(2022, 1, 4)))
+for link in url:
+    dr.get(link)
+    soup = BeautifulSoup(dr.page_source,'lxml')
+    tableMain = soup.find_all('table', {'class':'table datatable-striped text-center equity_prices_table datatable-with-sneak-peek js-anchor-price-table d-none d-lg-block dataTable no-footer'})
+    last_div = None
+    for last_div in tableMain:pass
+    if last_div:
+        table = last_div.getText()
+    df = pd.read_html(str(last_div), header=0)
+    df[0].rename(index= str, inplace = True)
+    frames.append(df[0].assign(date=datetime.datetime(2022, 1, 4)))
 
-# 3) Append all the information into a single data frame.
+3) Append all the information into a single data frame.
     
-# stock_list = pd.concat(frames)
-# stock_list = stock_list[['Name', 'Code', 'LACP']]
+stock_list = pd.concat(frames)
+stock_list = stock_list[['Name', 'Code', 'LACP']]
 
-# stock_list['date'] = date.today()
-# stock_list.info()
-# stock_list.to_csv(f'stock_price_{date.today()}.csv')
+stock_list['date'] = date.today()
+stock_list.info()
+stock_list.to_csv(f'stock_price_{date.today()}.csv')
 
 end = time.time()
 
 print("The time of execution of above program is :",
       (end-start), "s")
+
 
 
 
