ad80de4755f45f8ff2b9fb6ef3e4e6c424ba43a2
Amirul <amirulshafiq.dc@gmail.com>
2023-07-22 23:09:12 +0800

    test2

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import time
 from datetime import date
 import os
 from selenium.webdriver.chrome.service import Service as ChromeService
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from seleniumwire import webdriver
 
 
 chrome_options = webdriver.ChromeOptions()
