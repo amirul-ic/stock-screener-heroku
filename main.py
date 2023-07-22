19199dbd95717389363a52fb8ea4584723eb7751
Amirul <amirulshafiq.dc@gmail.com>
2023-07-22 20:37:47 +0800

    test2

--- main.py
+++ main.py
@@ -44,23 +44,23 @@ for link in url:
     dr.get(link)
     soup = BeautifulSoup(dr.page_source,'lxml')
     print (soup)
-#     tableMain = soup.find_all('table', {'class':'table datatable-striped text-center equity_prices_table datatable-with-sneak-peek js-anchor-price-table d-none d-lg-block dataTable no-footer'})
-#     last_div = None
-#     for last_div in tableMain:pass
-#     if last_div:
-#         table = last_div.getText()
-#     df = pd.read_html(str(last_div), header=0)
-#     df[0].rename(index= str, inplace = True)
-#     frames.append(df[0].assign(date=datetime.datetime(2022, 1, 4)))
+    tableMain = soup.find_all('table', {'class':'table datatable-striped text-center equity_prices_table datatable-with-sneak-peek js-anchor-price-table d-none d-lg-block dataTable no-footer'})
+    last_div = None
+    for last_div in tableMain:pass
+    if last_div:
+        table = last_div.getText()
+    df = pd.read_html(str(last_div), header=0)
+    df[0].rename(index= str, inplace = True)
+    frames.append(df[0].assign(date=datetime.datetime(2022, 1, 4)))
 
-# # 3) Append all the information into a single data frame.
+# 3) Append all the information into a single data frame.
     
-# stock_list = pd.concat(frames)
-# stock_list = stock_list[['Name', 'Code', 'LACP']]
+stock_list = pd.concat(frames)
+stock_list = stock_list[['Name', 'Code', 'LACP']]
 
-# stock_list['date'] = date.today()
-# stock_list.info()
-# stock_list.to_csv(f'stock_price_{date.today()}.csv')
+stock_list['date'] = date.today()
+stock_list.info()
+stock_list.to_csv(f'stock_price_{date.today()}.csv')
 
 end = time.time()
 
