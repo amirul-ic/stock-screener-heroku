da0fc2dcbad4c4baaaf73ba7be36d7d5b59e5686
amirul-ic <135077160+amirul-ic@users.noreply.github.com>
2023-08-15 12:00:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 from time import gmtime, strftime
 from datetime import datetime, timedelta
-# desired_time = datetime.strptime("03:00", "%H:%M")
-desired_time = datetime.now().strptime(strftime("%H:%M", gmtime()), "%H:%M")
+desired_time = datetime.strptime("04:00", "%H:%M")
+# desired_time = datetime.now().strptime(strftime("%H:%M", gmtime()), "%H:%M")
 
 def main():
     import timeit
@@ -472,7 +472,8 @@ while True:
         # seconds = difference.total_seconds()
         # print (f'Sleeping for {seconds} seconds')
         # time.sleep(abs(seconds)*0.1)
-        break
+        # break
+        exit()
 
 
 
