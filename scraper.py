656f7233485826b1a28672fb6153eb71aee53932
amirul-ic <135077160+amirul-ic@users.noreply.github.com>
2023-09-01 13:21:04 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -638,7 +638,7 @@ while True:
     # if desired_time == now:
     #     main()
     #     time.sleep(30)
-    if now == now:
+    if now == "test":
         main()
     else:
         # now = datetime.now().strptime(strftime("%H:%M", gmtime()), "%H:%M")
