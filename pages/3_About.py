27778ac8a9da83e4f6e4153b196d9fc47e12fe92
amirul-ic <135077160+amirul-ic@users.noreply.github.com>
2023-09-15 10:21:40 +0800

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -11,5 +11,24 @@ In September 2023, I decided to expand this project to do my self learning on ar
 st.markdown(multi)
 
 
+
+
+
+
+disclaimer = ''' **Disclaimer**
+
+This stock screener tool is provided solely for personal and educational purposes. It is not intended, nor should it be considered, as a source of financial advice or a tool for making investment decisions. The information and data presented by this stock screener are for informational purposes only and should not be used as a basis for any financial actions.
+
+Investing in financial markets carries inherent risks, and decisions related to buying, selling, or holding stocks should be made with careful consideration of your individual financial situation, risk tolerance, and investment goals. It is strongly recommended that you consult with a qualified financial advisor or conduct thorough research and analysis before making any financial decisions.
+
+The creators and providers of this stock screener do not endorse or guarantee the accuracy, completeness, or reliability of the data and information it presents. We shall not be held responsible for any financial losses, damages, or consequences that may arise from the use of this tool for making investment decisions.
+
+By using this stock screener, you acknowledge and agree to this disclaimer and absolve its creators and providers of any liability associated with your use of the tool for investment purposes. Always exercise caution and seek professional advice when dealing with financial matters.
+
+'''
+
+
+
+
 # st.markdown("# Page 3 🎉")
 # st.sidebar.markdown("# Page 3 🎉")
