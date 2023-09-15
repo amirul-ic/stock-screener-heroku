c5527fe8ecb54f2c84ba91d10cc9eb08f47bc351
amirul-ic <135077160+amirul-ic@users.noreply.github.com>
2023-09-15 10:13:47 +0800

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
-Hello everyone.
 
-multi = '''Late 2021, this project started on as an assignment for machine learning subject in postgraduate school. 
+
+multi = ''' Hello everyone.  
+
+Late 2021, this project started on as an assignment for machine learning subject in postgraduate school. 
 In September 2023, I decided to expand this project to do my self learning on area beyond my job scope.
 
 '''
