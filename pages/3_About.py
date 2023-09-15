755bddc181727b7d25e54a92fb67e3b30d98a700
amirul-ic <135077160+amirul-ic@users.noreply.github.com>
2023-09-15 10:03:19 +0800

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
+Hello everyone.
+
+multi = '''This project started on as an assignment for schools for machine learning related subject. 
+In September 2023, I decided to expand this project to do my self learning on area beyond my job scope.
+
+'''
+st.markdown(multi)
+
+
 # st.markdown("# Page 3 🎉")
 # st.sidebar.markdown("# Page 3 🎉")
