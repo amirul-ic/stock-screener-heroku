75e98fe4b7055054b1afcec30b76759f079a93e7
amirul-ic <135077160+amirul-ic@users.noreply.github.com>
2023-09-22 12:24:21 +0800

    Update 4_About.py

--- pages/4_About.py
+++ pages/4_About.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-st.subheader(':blue[Daily Shortlisted Stocks]')
+st.subheader(':blue[Background]')
 
 multi = ''' Hello everyone.  
 
