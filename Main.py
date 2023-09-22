8a764540bb88fce46234bd0594b47f1dc90fa483
amirul-ic <135077160+amirul-ic@users.noreply.github.com>
2023-09-22 09:27:50 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -41,8 +41,9 @@ try:
     df = get_UN_data()
     df['date'] = pd.to_datetime(df['date']).dt.date
     
-    min_date = datetime.datetime(2023,1,1)
-    max_date = datetime.date(2024,9,30)
+    # min_date = datetime.datetime(2023,1,1)
+    # max_date = datetime.date(2024,9,30)
+    # a_date = st.date_input("Pick date(s)", (min_date, max_date)) 
 
     st.subheader(':blue[Daily Shortlisted Stocks]')
 
@@ -50,7 +51,21 @@ try:
     #     st.write("This section list out the daily shortlisted stock")
 
     
-    a_date = st.date_input("Pick date(s)", (min_date, max_date)) 
+    today = datetime.datetime.now()
+    current_year = today.year
+    sep_7 = datetime.date(current_year, 9, 5)
+    dec_31 = datetime.date(current_year, 12, 31)
+
+    a_date = st.date_input(
+        "Select the shortlisted date",
+        (sep_7, datetime.date(current_year, 9, 7)),
+        sep_7,
+        dec_31,
+        format="YYYY/MM/DD",
+    )
+
+
+    
     options = st.multiselect("Choose scenario", sorted(df['scenario'].unique()))
 
     st.markdown(''' 
@@ -73,37 +88,37 @@ try:
         st.dataframe(data, width=1000000) 
 
 
-    st.markdown("***")
+    # st.markdown("***")
 
-    # st.subheader('A header with _italics_ :blue[colors] and emojis :sunglasses:')
-    st.subheader('Distribution of Shortlisted Stocks :blue[by scenario]')
+    # # st.subheader('A header with _italics_ :blue[colors] and emojis :sunglasses:')
+    # st.subheader('Distribution of Shortlisted Stocks :blue[by scenario]')
 
-    # tab1, tab2 = st.tabs(["Distribution (by scenario)", "Altair native theme"])
-    tab1 = st.tabs(["Distribution (by scenario)"])
-    with tab1:       
-        dfa = pd.crosstab(df['date'], df['scenario'], dropna=False)
-        st.bar_chart(dfa, width=10)
+    # # tab1, tab2 = st.tabs(["Distribution (by scenario)", "Altair native theme"])
+    # tab1 = st.tabs(["Distribution (by scenario)"])
+    # with tab1:       
+    #     dfa = pd.crosstab(df['date'], df['scenario'], dropna=False)
+    #     st.bar_chart(dfa, width=10)
     
-    # with tab2:
-    #     st.dataframe(df.style.highlight_max(axis=0))  # Same as st.write(df)
-    #     st.line_chart(df)
-    #         # data /= 1000000.0
-    #         # st.write("### Gross Agricultural Production ($B)", data.sort_index())
+    # # with tab2:
+    # #     st.dataframe(df.style.highlight_max(axis=0))  # Same as st.write(df)
+    # #     st.line_chart(df)
+    # #         # data /= 1000000.0
+    # #         # st.write("### Gross Agricultural Production ($B)", data.sort_index())
 
-    #         # data = data.T.reset_index()
-    #         # data = pd.melt(data, id_vars=["index"]).rename(
-    #         #     columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-    #         # )
-    #         # chart = (
-    #         #     alt.Chart(data)
-    #         #     .mark_area(opacity=0.3)
-    #         #     .encode(
-    #         #         x="year:T",
-    #         #         y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-    #         #         color="Region:N",
-    #         #     )
-    #         # )
-    #         # st.altair_chart(chart, use_container_width=True)
+    # #         # data = data.T.reset_index()
+    # #         # data = pd.melt(data, id_vars=["index"]).rename(
+    # #         #     columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+    # #         # )
+    # #         # chart = (
+    # #         #     alt.Chart(data)
+    # #         #     .mark_area(opacity=0.3)
+    # #         #     .encode(
+    # #         #         x="year:T",
+    # #         #         y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+    # #         #         color="Region:N",
+    # #         #     )
+    # #         # )
+    # #         # st.altair_chart(chart, use_container_width=True)
 
 except :
     st.error(
